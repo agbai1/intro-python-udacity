f6831e2df2a8c4ea4c91c1cf532c2d0c3f3b8dce
agbai1 <paulekeagbai@gmail.com>
2020-05-28 00:24:23 -0400

    Take out the print statement and clean out code

--- l4_break_the_string_quiz.py
+++ l4_break_the_string_quiz.py
@@ -1,5 +1,5 @@
 # HINT: modify the headlines list to verify your loop works with different inputs
-headlines = ["Local Bear Eaten by Man",
+headlines_list = ["Local Bear Eaten by Man",
              "Legislature Announces New Laws",
              "Peasant Discovers Violence Inherent in System",
              "Cat Rescues Fireman Stuck in Tree",
@@ -7,27 +7,19 @@ headlines = ["Local Bear Eaten by Man",
              "Papperbok Review: Totally Triffic"]
 
 news_ticker = ""
-# write your loop here
-
 j = 0
 
-
-for header in headlines:
+for headline in headlines_list:
     len_news_ticker = len(news_ticker)
-    print("Current length of news ticker is {} ".format(len(news_ticker)))
     if len_news_ticker > 140:
-        print("breaking loop now!")
         break
-    elif len_news_ticker + len(header) > 140:
+    elif len_news_ticker + len(headline) > 140:
         while(len(news_ticker)) < 140:
-            print("Length of news ticker is in While loop {} ".format(len(news_ticker)))
-            print("Current character being added {} ".format(header[j]))
-            news_ticker += header[j]
+            news_ticker += headline[j]
             j += 1
         continue
     else:
-        print("  adding {}".format(header))
-        news_ticker += header + " "
+        news_ticker += headline + " "
 
 
 print("\nNews Ticker is: {}".format(news_ticker))
